2fbd9552f83c08cced82d33ff1c133196b75352a
smerser <soren.merser@gmail.com>
2018-01-02 17:52:56 +0100

    Rename, rotation and thumbnails

--- png2gif.sh
+++ png2gif.sh
@@ -2,11 +2,15 @@
 # Using ImageMagick to create thumbnail gif files from image files
 # Quote arg if using wildcard characters e.g. ./JPG2ICO.sh '*.jpeg'
 
-# find . -iname "28_12__14_21_00.jpg" -maxdepth 1 -exec convert -auto-orient {} thumbs/{} \;
+# RENAME
+# ls *|sed 's/.....\(..\)-\(..\)\(..\).\(..\).\(.*\)/mv & \2_\1__\3_\4_\5/' | sh
+
+# ROTATE
+# find . -iname "*.jpg" -maxdepth 1 -exec convert --auto-orient {} thumbs/{} \;
 
 for file in $@
 do
-    convert $file --auto-orient # iPhone/iPad photos may display with incorrect rotatation
+    convert $file --auto-orient # iPhone/iPad photos may display with incorrect rotation
     name=$(echo $file| cut -d'.' -f1)
     #echo "Created: $name.ico"
     #convert -resize x16 -gravity center -crop 32x32+0+0 +repage $file -flatten -colors 256 -background transparent "./$name.ico"
